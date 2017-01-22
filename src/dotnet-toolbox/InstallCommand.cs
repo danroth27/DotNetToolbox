ca1791cf6bef89c4debca359410110418fe6854c
blackdwarf <zlakne@microsoft.com>
2017-01-21 20:56:31 -0800

    Trying to use the dotnet-add-package command

--- src/dotnet-toolbox/InstallCommand.cs
+++ src/dotnet-toolbox/InstallCommand.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Runtime.InteropServices;
+using System.Diagnostics;
+using NuGet.Configuration;
+using System.Linq;
 
 namespace DotNetToolbox
 {
@@ -12,10 +15,10 @@ namespace DotNetToolbox
     {
         private string _projectFile = @"<Project Sdk=""Microsoft.NET.Sdk"">
   <PropertyGroup>
-    <TargetFramework>netcoreapp1.1</TargetFramework>
-    <RootNamespace>DotNetToolbox</RootNamespace>
+    <TargetFramework>netcoreapp1.0</TargetFramework>
   </PropertyGroup>
 </Project>";
+        private string _shellScript = @"";
         public InstallCommand(CommandLineApplication parent)
         {
             Parent = parent;
@@ -35,6 +38,8 @@ namespace DotNetToolbox
         public async Task<int> Run()
         {
             var packageId = PackageArgument.Value;
+            var packageVersion = VersionOption.Value();
+            Out.WriteLine($"The package ID provided was {packageId}");
             var homeDir = (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) ? Environment.GetEnvironmentVariable("USERPROFILE") : Environment.GetEnvironmentVariable("HOME");
             var dotnet = Path.Combine(homeDir, ".dotnet");
             var projectFile = Path.Combine(dotnet, "globalTools.csproj");
@@ -53,8 +58,43 @@ namespace DotNetToolbox
             {
                 Error.WriteLine(e.Message);
             }
-            Out.WriteLine($"I wish I could install {packageId}, but I don't know how! :'(");
-            Out.WriteLine($"The home dir is {homeDir}");
+            // Out.WriteLine(nugetPackagePath);
+            // return 0;
+            var pi = Process.Start(new ProcessStartInfo {
+                FileName = "dotnet",
+                Arguments = $"add package --version {packageVersion} {packageId}",
+                WorkingDirectory = dotnet,
+                UseShellExecute = false
+            });
+            pi.WaitForExit();
+            if (pi.ExitCode != 0)
+            {
+                Error.WriteLine("The call to package failed");
+                Error.WriteLine(pi.StandardError.ReadToEnd());
+            }
+            // We have the package restored
+            var nugetPackagePath = NuGetPathContext.Create(settingsRoot: String.Empty).UserPackageFolder;
+            var fullPath = Path.Combine(nugetPackagePath, packageId, packageVersion, "lib", "netcoreapp1.0");
+            Out.WriteLine(fullPath);
+
+            // Find the dotnet-<foo> File
+            // foreach (var file in Directory.GetFiles(fullPath, "dotnet*.dll"))
+            // {
+            //     Out.WriteLine(file);
+            // }
+            var pathToTool = Directory.GetFiles(fullPath, "dotnet*.dll").FirstOrDefault();
+            if (string.IsNullOrEmpty(pathToTool))
+                throw new Exception("The package does not contain a dotnet-*.dll file");
+
+            // Now we have the thing and we need to execute it using dotnet to make sure it works
+            var p2 = Process.Start(new ProcessStartInfo {
+                FileName = "dotnet",
+                Arguments = $"--additionalprobingpath {nugetPackagePath} {fullPath}",
+            });
+            p2.WaitForExit();
+
+            // Out.WriteLine($"I wish I could install {packageId}, but I don't know how! :'(");
+            // Out.WriteLine($"The home dir is {homeDir}");
 
             return 0;
 
