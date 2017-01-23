99e39c71e78a394f65d41b247ed4612328ac2881
Zlatko Knezevic <zlakne@microsoft.com>
2017-01-22 18:25:08 -0800

    Done the MSBuild part

--- src/dotnet-toolbox/InstallCommand.cs
+++ src/dotnet-toolbox/InstallCommand.cs
@@ -14,12 +14,13 @@ namespace DotNetToolbox
 {
     public class InstallCommand : CommandLineApplication
     {
+        private const string _directory = ".toolbox";
+        private const string _projectFileName = "globaltools.csproj";
         private string _projectFile = @"<Project Sdk=""Microsoft.NET.Sdk"">
   <PropertyGroup>
     <TargetFramework>netcoreapp1.0</TargetFramework>
   </PropertyGroup>
 </Project>";
-        private string _shellScript = @"";
         public InstallCommand(CommandLineApplication parent)
         {
             Parent = parent;
@@ -46,37 +47,22 @@ namespace DotNetToolbox
             }
             var packageVersion = VersionOption.Value();
             Out.WriteLine($"The package ID provided was {packageId}");
-            var homeDir = (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) ? Environment.GetEnvironmentVariable("USERPROFILE") : Environment.GetEnvironmentVariable("HOME");
-            var dotnet = Path.Combine(homeDir, ".toolbox");
-            var projectFile = Path.Combine(dotnet, "globalTools.csproj");
-            try
-            {
-                if (!Directory.Exists(dotnet))
-                {
-                    Directory.CreateDirectory(dotnet);
-                }
-                if (!File.Exists(dotnet))
-                {
-                    File.WriteAllText(projectFile, _projectFile);
-                }
-            }
-            catch (Exception e)
-            {
-                Error.WriteLine(e.Message);
-            }
+
+            // Get the paths
+            var paths = GetDirAndProjectPaths();
+            EnsureProjectExists(paths);
             
             // Add the ItemGroup
-            var projectRoot = ProjectRootElement.Open(projectFile);
+            var projectRoot = ProjectRootElement.Open(paths.Item2);
             var itemGroup = projectRoot.AddItemGroup();
             itemGroup.AddItem("DotNetCliToolReference", packageId, new List<KeyValuePair<string, string>> { new KeyValuePair<string, string>("Version", packageVersion)});
             projectRoot.Save();
-            Out.WriteLine("Added the cli tool ref");
-            // return 1;
+            Out.WriteLine($"Added {packageId} to the store...");
             // Call restore 
             Out.WriteLine("Installing the package...");
             var restore = Process.Start(new ProcessStartInfo {
                 FileName = "dotnet",
-                Arguments = $"restore {projectFile}",
+                Arguments = $"restore {paths.Item2}",
                 RedirectStandardOutput = false,
                 RedirectStandardError = false
             });
@@ -85,7 +71,7 @@ namespace DotNetToolbox
             // We have the package restored
             var nugetPackagePath = NuGetPathContext.Create(settingsRoot: String.Empty).UserPackageFolder.TrimEnd(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar);
             var fullPath = Path.Combine(nugetPackagePath, packageId, packageVersion, "lib", "netcoreapp1.0");
-            Out.WriteLine(fullPath);
+            //Out.WriteLine(fullPath);
 
             // Find the dotnet-<foo> File
             var pathToTool = Directory.GetFiles(fullPath, "dotnet*.dll").FirstOrDefault();
@@ -95,17 +81,54 @@ namespace DotNetToolbox
             Out.WriteLine($"dotnet --additionalprobingpath {nugetPackagePath} {pathToTool}");
 
             // Now we have the thing and we need to execute it using dotnet to make sure it works
-            var p2 = Process.Start(new ProcessStartInfo
-            {
-                FileName = "dotnet",
-                Arguments = $"--additionalprobingpath {nugetPackagePath} {pathToTool}",
-            });
-            p2.WaitForExit();
+            //var p2 = Process.Start(new ProcessStartInfo
+            //{
+            //    FileName = "dotnet",
+            //    Arguments = $"--additionalprobingpath {nugetPackagePath} {pathToTool}",
+            //});
+            //p2.WaitForExit();
 
             // Out.WriteLine($"I wish I could install {packageId}, but I don't know how! :'(");
             // Out.WriteLine($"The home dir is {homeDir}");
-
+            Out.WriteLine("Putting the tool into the path...");
+            PutToolIntoPath(nugetPackagePath, pathToTool);
+            //var repo = Repository.Factory.GetCoreV3(NuGetConstants.V3FeedUrl);
+            //var resource = await repo.GetResourceAsync<MetadataResource>();
+            //resource.GetLatestVersion(string packageId, bool includePrerelease, bool includeUnlisted, ILogger log, CancellationToken token);
             return 0;
+
+        }
+
+        private void PutToolIntoPath(string nugetPackagePath, string pathToTool)
+        {
+        }
+
+        private Tuple<string, string> GetDirAndProjectPaths()
+        {
+            var homeDir = (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) ? Environment.GetEnvironmentVariable("USERPROFILE") : Environment.GetEnvironmentVariable("HOME");
+            var dotnet = Path.Combine(homeDir, _directory);
+            var projectFile = Path.Combine(dotnet, _projectFileName);
+            return new Tuple<string, string>(dotnet, projectFile);
+        }
+
+        private void EnsureProjectExists(Tuple<string, string> paths)
+        {
+            //var paths = GetDirAndProjectPaths();
+            try
+            {
+                if (!Directory.Exists(paths.Item1))
+                {
+                    Directory.CreateDirectory(paths.Item1);
+                }
+                if (!File.Exists(paths.Item2))
+                {
+                    File.WriteAllText(paths.Item2, _projectFile);
+                }
+            }
+            catch (Exception e)
+            {
+                Error.WriteLine(e.Message);
+            }
 
         }
     }
