00644826c4f1d5f9c38370853f0bee0a77d37b83
Daniel Roth <danroth27@hotmail.com>
2017-01-25 13:59:57 -0800

    Fix curl option for binary data

--- update-release.sh
+++ update-release.sh
@@ -1,2 +1,2 @@
-curl -X POST -H 'Content-Type:application/zip' --binary-data @dotnet-toolbox.zip https://uploads.github.com/repos/danroth27/DotNetToolbox/releases/5245515/assets?name=dotnet-toolbox.zip -u $1:
-curl -X POST -H 'Content-Type:application/zip' --binary-data @dotnet-toolbox.tar.gz https://uploads.github.com/repos/danroth27/DotNetToolbox/releases/5245515/assets?name=dotnet-toolbox.tar.gz -u $1:
+curl -X POST -H 'Content-Type:application/zip' --data-binary @dotnet-toolbox.zip https://uploads.github.com/repos/danroth27/DotNetToolbox/releases/5245515/assets?name=dotnet-toolbox.zip -u $1:
+curl -X POST -H 'Content-Type:application/zip' --data-binary @dotnet-toolbox.tar.gz https://uploads.github.com/repos/danroth27/DotNetToolbox/releases/5245515/assets?name=dotnet-toolbox.tar.gz -u $1:
