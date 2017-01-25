454939928dbd41aa212c33d3bbcede828f8cd1fe
Daniel Roth <danroth27@hotmail.com>
2017-01-25 12:20:51 -0800

    Move archiving to hosted build definition

--- docker-build.sh
+++ docker-build.sh
@@ -1,9 +1,5 @@
 #! /bin/sh
 set -e
-apt-get update
-apt-get -y install zip
 dotnet restore
 dotnet pack -c Release -o /src/packages
 dotnet publish -c Release -o /src/publish/dotnet-toolbox
-zip -r /src/publish/dotnet-toolbox /src/publish/dotnet-toolbox 
-tar -cvzf /src/publish/dotnet-toolbox.tar.gz /src/publish/dotnet-toolbox
