b1a7a35b47566de52475de98c5ce453fd2875fb3
Daniel Roth <danroth27@hotmail.com>
2017-01-25 15:24:04 -0800

    Delete update-release.sh

--- update-release.sh
+++
@@ -1,2 +0,0 @@
-curl -X POST -H 'Content-Type:application/zip' --data-binary @dotnet-toolbox.zip https://uploads.github.com/repos/danroth27/DotNetToolbox/releases/5245515/assets?name=dotnet-toolbox.zip -u $1:
-curl -X POST -H 'Content-Type:application/zip' --data-binary @dotnet-toolbox.tar.gz https://uploads.github.com/repos/danroth27/DotNetToolbox/releases/5245515/assets?name=dotnet-toolbox.tar.gz -u $1:
