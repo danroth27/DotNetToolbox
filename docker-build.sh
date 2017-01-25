b1ba96ed88bbebd7ed8c802c79b71c0e94f24e19
Daniel Roth <danroth27@hotmail.com>
2017-01-25 11:12:01 -0800

    Make apt-get install non-interactive

--- docker-build.sh
+++ docker-build.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 set -e
 apt-get update
-apt-get install zip
+apt-get -y install zip
 dotnet restore
 dotnet pack -c Release -o /src/packages
 dotnet publish -c Release -o /src/publish/dotnet-toolbox
