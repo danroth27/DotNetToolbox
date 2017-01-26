52a69fe36a92888a736c647fdbe58996281f5d72
Daniel Roth <danroth27@hotmail.com>
2017-01-25 22:40:59 -0800

    Update docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -5,8 +5,9 @@ dotnet pack -c Release -o /src/packages
 dotnet publish -c Release -o /src/publish/dotnet-toolbox
 apt-get update
 apt-get -y install zip
-zip -r /src/publish/dotnet-toolbox.zip /src/publish/dotnet-toolbox 
-tar -cvzf /src/publish/dotnet-toolbox.tar.gz /src/publish/dotnet-toolbox
+cd /src/publish/dotnet-toolbox
+zip -r /src/publish/dotnet-toolbox.zip *
+tar -cvzf /src/publish/dotnet-toolbox.tar.gz *
 apt-get -y install jq
 release=$(curl https://api.github.com/repos/danroth27/dotnettoolbox/releases/latest)
 asset_ids=$(echo $release | jq '.assets | .[] | .id')
@@ -14,7 +15,7 @@ for id in $asset_ids
 do
   curl -X DELETE https://api.github.com/repos/danroth27/dotnettoolbox/releases/assets/$id -u $GitHubToken:
 done
-cd /src/publish
 release_id=$(echo $release | jq .id)
+cd /src/publish
 curl -X POST -H 'Content-Type:application/zip' --data-binary @dotnet-toolbox.zip https://uploads.github.com/repos/danroth27/dotnettoolbox/releases/$release_id/assets?name=dotnet-toolbox.zip -u $GitHubToken:
 curl -X POST -H 'Content-Type:application/zip' --data-binary @dotnet-toolbox.tar.gz https://uploads.github.com/repos/danroth27/dotnettoolbox/releases/$release_id/assets?name=dotnet-toolbox.tar.gz -u $GitHubToken:
