2a0c4f72e2c6bdceed175156fc8b10c9660bb78e
Zlatko Knezevic <zlakne@microsoft.com>
2017-02-26 22:26:46 -0800

    Finished the uninstall command

--- src/dotnet-toolbox/UninstallCommand.cs
+++ src/dotnet-toolbox/UninstallCommand.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
+using DotNetToolbox.Helpers;
 using Microsoft.Extensions.CommandLineUtils;
+using Newtonsoft.Json;
 
 namespace DotNetToolbox
 {
@@ -11,7 +16,7 @@ namespace DotNetToolbox
         public UninstallCommand(CommandLineApplication parent)
         {
             Parent = parent;
-            Name = "install";
+            Name = "uninstall";
             PackageArgument = new PackageArgument(this);
             Arguments.Add(PackageArgument);
             OnExecute((Func<Task<int>>)Run);
@@ -32,25 +37,62 @@ namespace DotNetToolbox
             // 3. Remove the script named thus
             // 4. Remove the entry from the version file
             Out.WriteLine($"Attempting to uninstall {pkg.PackageId}");
-            var binaryName = GetBinaryNameForPackage(pkg.PackageId);
-            RemoveScriptBinding(binaryName);
-            Out.WriteLine($"{pkg.PackageId} uninstalled successfully!");
-            return 0;
+            try
+            {
+                var binaryName = String.Format("{0}{1}", GetBinaryNameForPackage(pkg.PackageId), GetExtension());
+                RemoveScriptBinding(binaryName);
+                RemoveFromVersionsFile(pkg.PackageId);
+                Out.WriteLine($"{pkg.PackageId} uninstalled successfully!");
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                this.Die(ex.Message, 100);
+                return 1;
+            }
         }
 
         private string GetBinaryNameForPackage(string packageId)
         {
-            throw new NotImplementedException();
+            // Go to the NuGet folder
+            // Find the package
+            // Find the version folder (need to get the info from the metadata file)
+            // Find the binary since it has to be called dotnet-<foo> and there has to be one
+            // return that mofo
+            // Delete from the versions file since it is dead, right?
+            //throw new NotImplementedException();
+            var installedVersion = GetInstalledVersionForPackageId(packageId);
+            var fullPath = Path.Combine(_toolboxConfig.NugetPackageRoot, packageId, installedVersion);
+            var fileName = Directory.GetFiles(fullPath, "dotnet-*.dll", SearchOption.AllDirectories).FirstOrDefault();
+            if (String.IsNullOrEmpty(fileName))
+                this.Die($"There is no dontet-*.dll binary associated with {packageId}.");
+            return Path.GetFileNameWithoutExtension(fileName);
         }
 
         private string GetExtension()
         {
-            return (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) ? ".cmd" : ".sh";
+            return (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) ? ".cmd" : "";
         }
 
         void RemoveScriptBinding(string binaryName)
         {
-            throw new NotImplementedException();
+            File.Delete(Path.Combine(_toolboxConfig.ToolboxDirectoryPath, binaryName));
+        }
+
+        void RemoveFromVersionsFile(string packageId)
+        {
+            var remainingTools = JsonConvert.DeserializeObject<List<PackageMetadata>>(File.ReadAllText(_toolboxConfig.VersionsFile))
+                                            .Where(p => !String.Equals(p.PackageId, packageId, StringComparison.OrdinalIgnoreCase)).ToList();
+            File.WriteAllText(_toolboxConfig.VersionsFile, JsonConvert.SerializeObject(remainingTools));
+        }
+
+        private string GetInstalledVersionForPackageId(string packageId)
+        {
+            var installedPackage = JsonConvert.DeserializeObject<List<PackageMetadata>>(File.ReadAllText(_toolboxConfig.VersionsFile))
+                                              .FirstOrDefault(p => p.PackageId == packageId);
+            if (installedPackage == null)
+                this.Die($"There is no entry for {packageId} in the metadata file.");
+            return installedPackage.RestoredVersion;
         }
     }
 }
